0b640f9f316a1655a9d97f2220f102c8e5e11cda
Vincent Simpson <34716206+vincentsimpson@users.noreply.github.com>
2019-01-27 15:39:07 -0500

    Road test v0.1.1

--- app/src/main/java/com/company/vince/physicswhiledriving/MainActivity.java
+++ app/src/main/java/com/company/vince/physicswhiledriving/MainActivity.java
@@ -361,7 +361,6 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         }
-
         if(trialNum == 6) {
             trials.add(trial1);
             trials.add(trial2);
